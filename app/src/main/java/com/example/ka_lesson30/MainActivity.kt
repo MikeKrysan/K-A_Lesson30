abca3b5da5fdab06413a59eb6e7078dfa5bec475
MikeKrysan <krysanmihasan@gmail.com>
2022-07-28 16:04:27 +0300

    second commit - Navigation View (lesson 31)

--- app/src/main/java/com/example/ka_lesson30/MainActivity.kt
+++ app/src/main/java/com/example/ka_lesson30/MainActivity.kt
@@ -2,25 +2,39 @@ package com.example.ka_lesson30
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import com.example.ka_lesson30.databinding.ActivityMainBinding
 
-//Чтобы добавить выдвижное меню, необходимо в activity_main.xml поменять сonstraintLayout контейнер на drawerLayout контейнер и добавить два элемента:
-//1.Основной контейнер, где будет содержатся весь контент экрана
-//2. Второй элемент, который будет выдвигаться, и у которого должен быть обязательный атрибут layout_gravity со значением "start"(выдвигается слева направо) либо "end" (выдвигается справа налево)
+//Для Navigation View:
+//1. Мы создали разметку для меню и создали Navigation View в nav_menu.xml. Но мы не указали что наша разметка будет находится внутри нашего navigation view
+//2. Чтобы совместить нашу разметку с NavigationView, идем в activity_main.xml и в NavigationView в атрибуте menu выбираем наше созданное nav_menu.xml из папки menu. Теперь я указал, какое меню будет использовать мой NavigationView
+
+//Cоздаем header для Navigation View:
+//создать отдельную разметку в layout -> layout resource file -> header.xml. Далее в разметке activity_main.xml в добавленном элементе создаем атрибут headerLayout и указываем на header.xml из папки layout
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        //1. Открываем из кода боковое меню:
+        setContentView(binding.root)
         binding.apply {
+            nv.setNavigationItemSelectedListener {
+                when(it.itemId) {
+                    R.id.item1 ->  Toast.makeText(this@MainActivity, "Item1", Toast.LENGTH_SHORT).show()
+
+                    R.id.item2 -> Toast.makeText(this@MainActivity, "Item2", Toast.LENGTH_SHORT).show()
+
+            }
+                drawer.closeDrawer(GravityCompat.START)     //мы хотим закрыть Navigation View в нем же по нажатию одного из item
+                true
+            }
             open.setOnClickListener{
-                drawer.openDrawer(GravityCompat.START)  //closeDrawer пишем в том случае, когда в выдвинутом меню нужно подтвердить проделанную операцию, и нажать на этом меню. Стандартно меню закрывается при нажатии на экране вне меню в любом месте экрана, либо самому задвинуть
+                drawer.openDrawer(GravityCompat.START)
             }
         }
     }
 }
+
